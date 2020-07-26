135d9138c444f52535c8dbc01775bee17e7e2298
= <jeffrey.menendez.castillo@gmail.com>
2020-07-26 10:44:21 -0600

    commit final de codigo

--- DecodificadorBiologia/src/decodificadorbiologia/DecodificadorBiologia.java
+++ DecodificadorBiologia/src/decodificadorbiologia/DecodificadorBiologia.java
@@ -1,4 +1,6 @@
+//Paquete del programa
 package decodificadorbiologia;
+//Import de librerias utiles
 
 import java.util.Scanner;
 
@@ -9,14 +11,12 @@ import java.util.Scanner;
  */
 public class DecodificadorBiologia {
 
+    //Variables estaticas
     static Scanner teclado = new Scanner(System.in);
-    static String secuencia1, secuencia2, resultadofinal = "";
-    static StringBuffer respuesta = new StringBuffer();
-    static int contadorcoincidencias = 0, tamanio = 0, contadorcoincidencias2 = 0;
+    static String secuencia1, secuencia2, resultadofinal = "", resultadopendiente="";
+    static int contadorcoincidencias = 0, contadorcoincidencias2 = 0;
 
-    /**
-     * @param args the command line arguments
-     */
+    //Metodo Main
     public static void main(String[] args) {
         System.out.println("Decodificador de ADN");
         System.out.println("Ingrese la primera secuencia de ADN");
@@ -26,29 +26,44 @@ public class DecodificadorBiologia {
         char array1[] = secuencia1.toCharArray();
         char array2[] = secuencia2.toCharArray();
         while (contadorcoincidencias2 < array1.length) {
+            //Reinicio de resultado y contadores
             contadorcoincidencias = 0;
-            respuesta.delete(0, respuesta.length());
+            resultadopendiente = "";
+            //Inicio de ciclos
             for (int i = contadorcoincidencias2; i < array1.length; i++) {
                 for (int j = contadorcoincidencias; j < array2.length; j++) {
+                    //Comparacion si encuentra alguna coincidencia
                     if (array1[i] == array2[j]) {
-                        respuesta = respuesta.append(array1[i]);
+                        resultadopendiente = resultadopendiente + String.valueOf(array1[i]);
                         contadorcoincidencias = j + 1;
                         break;
-                    } else {
+                    } else { //Comparacion si no encuentra ninguna coincidencia
 
-                        if (respuesta.length() > resultadofinal.length()) {
-                            resultadofinal = respuesta.toString();
+                        if (resultadopendiente.length() > resultadofinal.length()) {
+                            resultadofinal = resultadopendiente;
                         }
-                        respuesta.delete(0, respuesta.length());
+                        //Reinicio de resultado y contadores
+                        resultadopendiente = "";
                         contadorcoincidencias = 0;
                     }
                 }
             }
+            //Comparacion para obtener el resultado mayor
             contadorcoincidencias2++;
-            if (respuesta.length() > resultadofinal.length()) {
-                resultadofinal = respuesta.toString();
+            if (resultadopendiente.length() > resultadofinal.length()) {
+                resultadofinal = resultadopendiente;
             }
         }
-        System.out.println("La respuesta es " + resultadofinal);
+        limpiar();
+        //Resultado
+        System.out.println("El conjunto ordenado de bases adyacentes de mayor tamaño entre los ADNS es " + "'" + resultadofinal + "'");
+    }
+
+    //Metodo para limpiar pantalla
+    public static void limpiar() {
+        //Caracteres por imprimir
+        System.out.print("\033[H\033[2J");
+        //Metodo flush para imprimir los caracteres en pantalla
+        System.out.flush();
     }
 }
